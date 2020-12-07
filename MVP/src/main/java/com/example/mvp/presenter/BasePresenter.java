ee61cbc34e74de2cffb6a3b54c7c9d6c51212528
99034303 <99034303@qq.com>
2020-12-07 19:01:14 +0800

    实训项目

--- MVP/src/main/java/com/example/mvp/presenter/BasePresenter.java
+++ MVP/src/main/java/com/example/mvp/presenter/BasePresenter.java
@@ -3,7 +3,8 @@ package com.example.mvp.presenter;
 import com.example.mvp.model.IModel;
 import com.example.mvp.view.IView;
 
-public class BasePresenter<M extends IModel,V extends IView> implements IPresenter {
+//p层抽象类
+public abstract class BasePresenter<M extends IModel,V extends IView> implements IPresenter {
     protected M mModel;
     protected V mView;
 
