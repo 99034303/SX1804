ac7168b4a5ec6b4d70ee3b55b2ab5e632aef4aff
chichichi <3345123200@qq.com>
2020-12-07 20:48:22 +0800

    添加框架

---
+++ sp/src/main/java/com/wmc/sp/SPUtils.java
@@ -0,0 +1,90 @@
+package com.wmc.sp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Map;
+
+/**
+ * @author 魏铭池
+ */
+public class SPUtils {
+
+    private final SharedPreferences sharedPreferences;
+    private final SharedPreferences.Editor edit;
+
+    @SuppressLint("CommitPrefEdits")
+    public SPUtils(String name, Context context) {
+        sharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE);
+        edit = sharedPreferences.edit();
+    }
+
+    /**
+     * 存入数据
+     * @param key 键
+     * @param value 值
+     */
+    public void put(String key,Object value){
+        if (value instanceof String){
+            edit.putString(key,String.valueOf(value));
+        }else if (value instanceof Integer){
+            edit.putInt(key, (Integer) value);
+        }else if (value instanceof Boolean){
+            edit.putBoolean(key, (Boolean) value);
+        }else if (value instanceof Float){
+            edit.putFloat(key, (Float) value);
+        }else if (value instanceof Long){
+            edit.putLong(key, (Long) value);
+        }else {
+            edit.putString(key,value.toString());
+        }
+        //保存
+        edit.apply();
+    }
+
+
+    /**
+     * @param key 键
+     * @param defValue 值
+     * @return 返回获取到的数据
+     */
+    public Object get(String key,Object defValue){
+        if (defValue instanceof String){
+            return sharedPreferences.getString(key, (String) defValue);
+        }else if (defValue instanceof Integer){
+            return sharedPreferences.getInt(key, (Integer) defValue);
+        }else if (defValue instanceof Boolean){
+            return sharedPreferences.getBoolean(key, (Boolean) defValue);
+        }else if (defValue instanceof Float){
+            return sharedPreferences.getFloat(key, (Float) defValue);
+        }else if (defValue instanceof Long){
+            return sharedPreferences.getLong(key, (Long) defValue);
+        }else {
+            return null;
+        }
+    }
+
+    /**
+     * @return 返回获取到的所有数据
+     */
+    public Map<String, ?> getAll(){
+        return sharedPreferences.getAll();
+    }
+
+    /**
+     * 删除所有数据
+     */
+    public void deleteAll(){
+        edit.clear().apply();
+    }
+
+    /**
+     * 删除
+     * @param key 指定键
+     */
+    public void delete(String key){
+        edit.remove(key).apply();
+    }
+
+}
