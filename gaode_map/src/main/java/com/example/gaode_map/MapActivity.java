30b0e7f671e4b11f0893832801af83ea2e720e69
99034303 <99034303@qq.com>
2020-12-12 11:17:18 +0800

    增加网络地图id显示自定义地图样式

--- gaode_map/src/main/java/com/example/gaode_map/MapActivity.java
+++ gaode_map/src/main/java/com/example/gaode_map/MapActivity.java
@@ -40,8 +40,11 @@ public class MapActivity extends AppCompatActivity {
         if (customMapStyleOptions==null){
             customMapStyleOptions = new CustomMapStyleOptions()
                     .setEnable(true)
-                    .setStyleDataPath("/mnt/sdcard/style.data")
-                    .setStyleExtraPath("/mnt/sdcard/style_extra.data");
+                    //利用网络地图的样式id加载自定义样式
+                    .setStyleId("81db29be347e356f6cdc6eace17acede");
+//                    //根据本地的离线文件加载自定义样式
+//                    .setStyleDataPath("/mnt/sdcard/style.data")
+//                    .setStyleExtraPath("/mnt/sdcard/style_extra.data");
         }
         aMap.setCustomMapStyle(customMapStyleOptions);
 
