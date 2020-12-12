53b8dd2644ff66983993986f2a7cf930823a0b61
99034303 <99034303@qq.com>
2020-12-12 11:26:09 +0800

    网络地图id显示自定义样式

--- gaode_map/src/main/java/com/example/gaode_map/MapActivity.java
+++ gaode_map/src/main/java/com/example/gaode_map/MapActivity.java
@@ -40,8 +40,11 @@ public class MapActivity extends AppCompatActivity {
         if (customMapStyleOptions==null){
             customMapStyleOptions = new CustomMapStyleOptions()
                     .setEnable(true)
-                    .setStyleDataPath("/mnt/sdcard/style.data")
-                    .setStyleExtraPath("/mnt/sdcard/style_extra.data");
+                    //根据网络地图显示id样式
+                    .setStyleId("81db29be347e356f6cdc6eace17acede");
+//                    //加载本地
+//                    .setStyleDataPath("/mnt/sdcard/style.data")
+//                    .setStyleExtraPath("/mnt/sdcard/style_extra.data");
         }
         aMap.setCustomMapStyle(customMapStyleOptions);
 
