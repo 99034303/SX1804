cf98fd4f2c8f510204bc93eb69e3d8f206a73571
1918516546 <1918516546@qq.com>
2020-12-19 09:27:52 +0800

    初始化联系人模块

--- usercenter/src/main/java/com/wmc/usercenter/view/ContactsActivity.java
+++ usercenter/src/main/java/com/wmc/usercenter/view/ContactsActivity.java
@@ -9,6 +9,7 @@ import android.view.ViewStub;
 import android.widget.ImageView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.mvp.view.BaseMVPActivity;
 import com.example.net.BaseEntity;
