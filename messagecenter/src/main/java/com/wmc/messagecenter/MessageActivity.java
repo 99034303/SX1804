bc80932fe30f260bb6d0eab7a8360d57fa76d1a4
chichichi <3345123200@qq.com>
2020-12-21 11:02:29 +0800

    消息MVP框架

--- messagecenter/src/main/java/com/wmc/messagecenter/MessageActivity.java
+++ messagecenter/src/main/java/com/wmc/messagecenter/MessageActivity.java
@@ -1,13 +1,49 @@
 package com.wmc.messagecenter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.lifecycle.Observer;
 
-public class MessageActivity extends AppCompatActivity {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.mvp.view.BaseMVPActivity;
+import com.wmc.messagecenter.mvp.model.entity.Entity;
+import com.wmc.messagecenter.mvp.model.entity.SingleEntity;
+import com.wmc.messagecenter.mvp.presenter.UCPresenter;
+
+import io.reactivex.functions.Consumer;
+
+public class MessageActivity extends BaseMVPActivity<UCPresenter> {
+    @Override
+    protected void bindView() {
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
+    protected void createPresenter() {
+        mPresenter= new UCPresenter(this);
+    }
+
+    @SuppressLint("CheckResult")
+    @Override
+    protected void initData() {
+        mPresenter.getSingleMessage(new SingleEntity()).subscribe(new Consumer<Entity<SingleEntity>>() {
+            @Override
+            public void accept(Entity<SingleEntity> singleEntityEntity) throws Exception {
+
+            }
+        });
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_message;
     }
 }
--- messagecenter/src/main/java/com/wmc/messagecenter/mvp/contract/UserCenter.kt
+++ messagecenter/src/main/java/com/wmc/messagecenter/mvp/contract/UserCenter.kt
@@ -1,14 +1,21 @@
 package com.wmc.messagecenter.mvp.contract
 
+import com.example.mvp.model.IModel
+import com.example.mvp.presenter.BasePresenter
 import com.example.mvp.view.IView
+import com.wmc.messagecenter.mvp.model.entity.Entity
+import com.wmc.messagecenter.mvp.model.entity.SingleEntity
+import io.reactivex.Flowable
 
 interface UserCenter {
-    interface UserCenterView : IView{
-        fun success() {
-
-        }
-        fun failed() {
-
-        }
+    abstract class UserCenterModel : IModel{
+        abstract fun sendSingleMessage(singleEntity: SingleEntity):Flowable<Entity<SingleEntity>>
+        abstract fun getSingleMessage(singleEntity: SingleEntity):Flowable<Entity<SingleEntity>>
     }
+
+    abstract class UserCenterPresenter(mView: IView) : BasePresenter<UserCenterModel, IView>(mView){
+        abstract fun sendSingleMessage(singleEntity: SingleEntity): Flowable<Entity<SingleEntity>>
+        abstract fun getSingleMessage(singleEntity: SingleEntity): Flowable<Entity<SingleEntity>>
+    }
+
 }
